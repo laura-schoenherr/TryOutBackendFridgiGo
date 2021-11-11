f806f9a96b39d618aa6bb129deda9d85bd7fe99c
laura-schoenherr <lauraschoenherr@outlook.de>
2021-11-11 09:17:19 +0100

    First Commit with Greeting API, sends User and id, just for trying out how to connect Back and Frontend

---
+++ src/main/java/com/example/eigeneapi/EigeneApiApplication.java
@@ -0,0 +1,28 @@
+package com.example.eigeneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@SpringBootApplication
+public class EigeneApiApplication {
+
+    public static void main(String[] args) {SpringApplication.run(EigeneApiApplication.class, args);}
+
+        @RestController
+        public class GreetingController {
+            private static final String template = "Hello, %s!";
+            private final AtomicLong counter = new AtomicLong();
+
+            @GetMapping("/greeting")
+            @CrossOrigin(origins = "http://localhost:8100")
+            public Greeting greeting(@RequestParam(value = "name", defaultValue = "User") String name) {
+                return new Greeting(counter.incrementAndGet(), String.format(template, name));
+            }
+    }
+}
---
+++ src/test/java/com/example/eigeneapi/EigeneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eigeneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EigeneApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
