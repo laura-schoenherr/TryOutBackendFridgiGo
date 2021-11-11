bef1e3125934b5ce2c2d608fc21754f1cb656a93
laura-schoenherr <lauraschoenherr@outlook.de>
2021-11-11 11:12:36 +0100

    add Recipe class with title and id

--- src/main/java/com/example/eigeneapi/DataBase.java
+++ src/main/java/com/example/eigeneapi/DataBase.java
@@ -1,9 +1,58 @@
 package com.example.eigeneapi;
 
-
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class DataBase {
 
     //Diese Funktion persistiert ein ihr übergebenes Objekt:
+    public static void Persist(Object Eingabe){
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
+
+        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+        SessionFactory factory = meta.getSessionFactoryBuilder().build();
+        Session session = factory.openSession();
+
+        session.beginTransaction();
+        session.persist(Eingabe);
+        session.flush();
+        session.close();
+    }
+
+    /*Diese Funktion liest das Objekt mit der ihr übergebenen id ein und gibt es als "Student"-Objekt aus:
+    public static Student CallStudentById(Integer input){
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
+
+        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+        SessionFactory factory = meta.getSessionFactoryBuilder().build();
+        Session session = factory.openSession();
+
+
+        session.beginTransaction();
+        Student student = session.load(Student.class, input.longValue());
+        session.flush();
+        return student;
+    }
+
+    //Diese Funktion updatet das ihr übergebene Objekt:
+    public static void EvictAndMerge(Object objekt, Integer id){
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
+
+        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+        SessionFactory factory = meta.getSessionFactoryBuilder().build();
+        Session session = factory.openSession();
+
+
+        session.beginTransaction();
+        session.evict(session.load(Student.class, id.longValue()));
+        session.flush();
+        session.merge(objekt);
+        session.flush();
+        session.close();
+    }*/
 
 }
--- src/main/java/com/example/eigeneapi/Recipe.java
+++ src/main/java/com/example/eigeneapi/Recipe.java
@@ -1,5 +1,30 @@
 package com.example.eigeneapi;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Recipe {
+
+    private String recipeTitle;
+    private long id;
+
+    public Recipe() {
+    }
+
+    public String getRecipeTitle() {
+        return recipeTitle;
+    }
+
+    public void setRecipeTitle(String recipeTitle) {
+        this.recipeTitle = recipeTitle;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Id
+    public long getId() {return id;}
 
 }
